a8e098009b3c46de0b05e3d17aae20d64a0c3d13
JulianCortes0 <jscortesg@unbosque.edu.co>
2023-12-11 13:06:21 -0500

    version 2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -15,10 +15,10 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>{props.quantity}</td>
         <td>{Location}{parseInt(props.unitprice)}</td>
-        <td>{Location}{parseInt(props.quantity)*parseInt(props.unitprice)}</td>
-        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td>
+        {/* <td>{props.quantity}</td>
+        <td>{Location}{parseInt(props.quantity)*parseInt(props.unitprice)}</td> */}
+        {/* <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td> */}
         </tr>
     );
 };
